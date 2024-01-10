515e60255a5fe925e161b41c98d68d1af9694378
syndromowicz <kleofasx@gmail.com>
2024-01-10 19:20:33 +0100

    solution

---
+++ src/main/java/pl/javastart/task/Ingridient.java
@@ -0,0 +1,6 @@
+package pl.javastart.task;
+
+public class Ingridient {
+    String name;
+    int volume;
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,5 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
     }
 }
