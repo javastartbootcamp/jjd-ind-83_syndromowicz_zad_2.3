7a838b4afffe44cf5f113a3790d742908c3e7a22
syndromowicz <kleofasx@gmail.com>
2024-01-11 14:11:50 +0100

    poprawka

--- src/main/java/pl/javastart/task/Drink.java
+++ src/main/java/pl/javastart/task/Drink.java
@@ -3,27 +3,7 @@ package pl.javastart.task;
 import java.sql.SQLOutput;
 
 public class Drink {
-    public static void main(String[] args) {
-        String drink = "Mojito";
-        Boolean alcohol = true;
-        double price = 13.0;
-        Ingridient ingridient1 = new Ingridient();
-        ingridient1.name = "woda";
-        ingridient1.volume = 100;
-        Ingridient ingridient2 = new Ingridient();
-        ingridient2.name = "wódka";
-        ingridient2.volume = 50;
-        Ingridient ingridient3 = new Ingridient();
-        ingridient3.name = "sok cytrynowy";
-        ingridient3.volume = 75;
-        int totalVolume = (ingridient1.volume + ingridient2.volume + ingridient3.volume);
-        System.out.println("Drink: " + drink);
-        System.out.println("Cena " + price);
-        System.out.println("Czy zawiera alkohol ? " + alcohol);
-        System.out.println("Składniki:");
-        System.out.println(ingridient1.name);
-        System.out.println(ingridient2.name);
-        System.out.println(ingridient3.name);
-        System.out.println("Pojemność: " + totalVolume + "ml");
-    }
+    String name;
+    double price;
+    boolean alcohol;
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,5 +3,27 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Drink drink1 = new Drink();
+        drink1.price = 13.0;
+        drink1.alcohol = true;
+        drink1.name = "Mojito";
+        Ingridient ingridient1 = new Ingridient();
+        ingridient1.name = "woda";
+        ingridient1.volume = 100;
+        Ingridient ingridient2 = new Ingridient();
+        ingridient2.name = "wódka";
+        ingridient2.volume = 50;
+        Ingridient ingridient3 = new Ingridient();
+        ingridient3.name = "sok cytrynowy";
+        ingridient3.volume = 75;
+        int totalVolume = (ingridient1.volume + ingridient2.volume + ingridient3.volume);
+        System.out.println("Drink: " + drink1.name);
+        System.out.println("Cena " + drink1.price + "zł");
+        System.out.println("Czy zawiera alkohol ? " + drink1.alcohol);
+        System.out.println("Składniki:");
+        System.out.println(ingridient1.name);
+        System.out.println(ingridient2.name);
+        System.out.println(ingridient3.name);
+        System.out.println("Pojemność: " + totalVolume + "ml");
     }
 }
